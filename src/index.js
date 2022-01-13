3a21f5ca39cc6f3524d6d4de51abe1fc3c744410
chafer <olegshteyn@gmail.com>
2022-01-13 20:44:53 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const startGame = (initQuestionAnswer) => {
       questionNumber += 1;
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
